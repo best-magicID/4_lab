62451ed9b209155980fd1c38179b1496ba9d9069
Andrey <58469691+best-magicID@users.noreply.github.com>
2020-07-01 15:41:32 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.IO;
+
+namespace Лаба4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GetChanel.Chanel("2020 - 07 - 02");//"2020 - 07 - 03"//Можно получать расписание на разные даты
+        }
+    }
+}
