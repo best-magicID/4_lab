df0f5e3eb983e264010ba6377d999907ef5d4609
Andrey <58469691+best-magicID@users.noreply.github.com>
2020-07-01 15:40:31 +0500

    Create Show.cs

---
+++ Show.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба4
+{
+    class Show
+    {
+        public string title { get; set; }
+        public string time { get; set; }
+        public string link { get; set; }
+    }
+}
